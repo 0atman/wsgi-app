f4c9321f733b8fb4522d48d18836d603cd24f0ae
Robin Winslow <robin.winslow@canonical.com>
2014-07-07 16:04:38 +0100

    Implement pgsql and wsgi-file hooks in python

--- hooks/helpers.py
+++ hooks/helpers.py
@@ -1,5 +1,37 @@
+# System
+import json
 from os import path, pardir
 from urlparse import urlunparse
+
+# Globals
+env_vars_json_filename = 'env.json'
+
+
+def set_env(cache_dir, env_vars):
+    json_file_path = path.join(cache_dir, env_vars_json_filename)
+
+    with open(json_file_path, 'w') as json_file:
+        json_file.write(json.dumps(env_vars))
+
+
+def set_env_var(cache_dir, key, value):
+    env_vars = get_env(cache_dir)
+
+    env_vars[key] = value
+
+    set_env(cache_dir, env_vars)
+
+
+def get_env(cache_dir):
+    json_file_path = path.join(cache_dir, env_vars_json_filename)
+
+    env_vars = {}
+
+    if path.isfile(json_file_path):
+        with open(json_file_path) as json_file:
+            env_vars = json.loads(json_file.read())
+
+    return env_vars
 
 
 def parent_dir(dir_path):
@@ -15,12 +47,14 @@ def build_url(
     path='', params='', query='', fragment=''
 ):
     return urlunparse(
-        scheme,
-        build_url_host(domain, port, username, password),
-        path,
-        params,
-        query,
-        fragment
+        (
+            scheme,
+            build_url_host(domain, port, username, password),
+            path,
+            params,
+            query,
+            fragment
+        )
     )
 
 
@@ -41,3 +75,60 @@ def build_url_host(domain, port=None, username=None, password=None):
         host = "{0}@{1}".format(credentials, host)
 
     return host
+
+
+def ansible_config(charm_dir, config_data):
+    '''
+    Collect config settings from an ansible role
+    by reading both 'defaults/main.yml'
+    and 'vars/main.yml'
+    and appending their data to config_data
+    '''
+
+    # Local imports - 'cos they weren't ready earlier
+    from jinja2 import Environment, FileSystemLoader
+
+    # Setup template parser environment
+    wsgi_role_path = path.join(charm_dir, 'roles', 'wsgi-app')
+    template_env = Environment(loader=FileSystemLoader(wsgi_role_path))
+
+    config_data = add_yaml_to_config(
+        'defaults/main.yml', template_env, config_data
+    )
+
+    config_data = add_yaml_to_config(
+        'vars/main.yml', template_env, config_data
+    )
+
+    return config_data
+
+
+def add_yaml_to_config(template_path, template_env, config_data):
+    import yaml
+    from jinja2 import meta, UndefinedError
+
+    # Find variables in template
+    content = template_env.loader.get_source(template_env, template_path)
+    template_parser = template_env.parse(content[0])
+    template_variables = meta.find_undeclared_variables(template_parser)
+
+    # Find undeclared variables not already in context
+    undefined_variables = list(set(template_variables) - set(config_data))
+
+    # Parse template
+    template = template_env.get_template(template_path)
+    yaml_content = template.render(config_data)
+    new_variables = yaml.load(yaml_content)
+    config_data.update(new_variables)
+
+    # If any new variables were in the undeclared variables
+    list_intersection = filter(
+        lambda x: x in undefined_variables, new_variables
+    )
+
+    if list_intersection:
+        add_yaml_to_config(template_path, template_env, config_data)
+    elif undefined_variables:
+        raise UndefinedError('Undefined variables: ' + str(undefined_variables))
+
+    return config_data
--- hooks/hooks.py
+++ hooks/hooks.py
@@ -2,13 +2,27 @@
 
 # System
 import sys
+from shutil import rmtree
+from os import path, mkdir
+from datetime import datetime
 
 # Local
 import charmhelpers.contrib.ansible
-from helpers import build_url
-from charmhelpers.core.hookenv import (
-    relation_get
+from helpers import (
+    build_url, parent_dir, ansible_config,
+    set_env_var, get_env, set_env
 )
+from charmhelpers.core.hookenv import (
+    relation_get, relation_set,
+    open_port, close_port,
+    config, local_unit,
+    relations, relation_ids
+)
+from charmhelpers.core.host import log
+
+# Globals (unfortunately)
+charm_dir = parent_dir(__file__)
+cache_dir = path.join(charm_dir, 'charm_cache')
 
 # Create the hooks helper which automatically registers the
 # required hooks based on the available tags in your playbook.
@@ -19,14 +33,71 @@ hooks = charmhelpers.contrib.ansible.AnsibleHooks(
 )
 
 
+@hooks.hook('wsgi-file-relation-changed')
+def wsgi_relation_joined_changed():
+    template_vars = config()
+    template_vars['local_unit'] = local_unit()
+    log('Config options: ' + str(template_vars))
+    ansible_data = ansible_config(charm_dir, template_vars)
+    log('Ansible config options: ' + str(ansible_data))
+
+    log_file_path = path.join(
+        ansible_data['log_dir'],
+        ansible_data['app_label'] + '-access.log'
+    )
+
+    env_dictionary = get_env(cache_dir)
+    env_list = ["{0}={1}".format(k, v) for k, v in env_dictionary.items()]
+    env_string = " ".join(env_list)
+
+    wsgi_relation_settings = {
+        'project_name': ansible_data.get('app_label', ''),
+        'working_dir': path.join(ansible_data.get('code_dir', ''), 'current'),
+        'python_path': ansible_data.get('python_path', ''),
+        'wsgi_user': ansible_data.get('wsgi_user', ''),
+        'wsgi_group': ansible_data.get('wsgi_group', ''),
+        'port': ansible_data.get('listen_port', ''),
+        'wsgi_access_logfile': log_file_path,
+        'wsgi_wsgi_file': ansible_data.get('wsgi_application', ''),
+        'wsgi_extra': '--error-logfile=' + log_file_path,
+        'env_extra': env_string,
+        'timestamp': datetime.now().isoformat()
+    }
+
+    log('wsgi relation settings: ' + str(wsgi_relation_settings))
+
+    # Set these settings on any wsgi-file relations
+    for relation_id in relation_ids('wsgi-file'):
+        relation_set(
+            relation_id=relation_id,
+            **wsgi_relation_settings
+        )
+
+    log('Relation settings: ' + str(relation_get()))
+
+    open_port(ansible_data['listen_port'])
+
+
+@hooks.hook('wsgi-relation-broken')
+def wsgi_relation_broken():
+    template_vars = config()
+    template_vars['local_unit'] = local_unit()
+    ansible_data = ansible_config(charm_dir, template_vars)
+
+    close_port(ansible_data['listen_port'])
+
+
 @hooks.hook('pgsql-relation-broken')
 def pgsql_relation_broken():
-    # Remove the postgres environment variable
-    NotImplementedError(
-        '''
-        Clear the database URL environment variable
-        '''
-    )
+    env_vars = get_env(cache_dir)
+
+    if 'DATABASE_URL' in env_vars:
+        del env_vars['DATABASE_URL']
+
+        set_env(env_vars)
+
+        # Reset wsgi relation settings
+        wsgi_relation_joined_changed()
 
 
 @hooks.hook('pgsql-relation-joined', 'pgsql-relation-changed')
@@ -36,9 +107,9 @@ def pgsql_relation():
 
     if database_name and database_host:
         # Prepare the charm for postgres database relation
-        # By putting database settings in a python settings file
+        # By putting database settings in environment variables
 
-        build_url(
+        database_url = build_url(
             scheme='postgresql',
             domain=database_host,
             port=relation_get("port"),
@@ -47,12 +118,10 @@ def pgsql_relation():
             path=database_name
         )
 
-    NotImplementedError(
-        '''
-        Set the environment variable
-        for the database URL in gunicorn
-        '''
-    )
+        set_env_var(cache_dir, 'DATABASE_URL', database_url)
+
+        # Reset wsgi relation settings
+        wsgi_relation_joined_changed()
 
 
 @hooks.hook('install', 'upgrade-charm')
@@ -63,6 +132,14 @@ def install():
     function finishes, any tasks in the playbook tagged with install are
     executed.
     """
+
+    # Recreate cache directory
+    if path.isdir(cache_dir):
+        rmtree(cache_dir)
+
+    mkdir(cache_dir)
+
+    # Setup ansible
     charmhelpers.contrib.ansible.install_ansible_support(from_ppa=True)
 
 
