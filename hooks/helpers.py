ec5f3c96cbd492ba1bb80bbbc8b0c47084f30535
Robin Winslow <robin.winslow@canonical.com>
2014-07-08 14:28:02 +0100

    Add update target task as python function; default update target to empty; make pgsql and wsgi-file relations run on config-changed, and hopefully make them more robust

--- hooks/helpers.py
+++ hooks/helpers.py
@@ -2,28 +2,44 @@
 import json
 from os import path, pardir
 from urlparse import urlunparse
-
-# Globals
-env_vars_json_filename = 'env.json'
+from collections import namedtuple
 
 
-def set_env(cache_dir, env_vars):
-    json_file_path = path.join(cache_dir, env_vars_json_filename)
+def literal(**kw):
+    """
+    Create an "object literal"
+    by creating a named tuple with keyword arguments
+    """
+
+    return namedtuple('literal', kw)(**kw)
+
+
+def save_to_json_file(json_file_path, data_to_save):
+    """
+    Save a python object as json in a file
+    """
 
     with open(json_file_path, 'w') as json_file:
-        json_file.write(json.dumps(env_vars))
+        json_file.write(json.dumps(data_to_save))
 
 
-def set_env_var(cache_dir, key, value):
-    env_vars = get_env(cache_dir)
+def update_property_in_json_file(json_file_path, key, value):
+    """
+    Given a path to a file containing a JSON object
+    Set the value of a property on that JSON object
+    """
 
-    env_vars[key] = value
+    json_data = parse_json_file(json_file_path)
 
-    set_env(cache_dir, env_vars)
+    json_data[key] = value
+
+    save_to_json_file(json_file_path, json_data)
 
 
-def get_env(cache_dir):
-    json_file_path = path.join(cache_dir, env_vars_json_filename)
+def parse_json_file(json_file_path):
+    """
+    I think the name says it all...
+    """
 
     env_vars = {}
 
@@ -35,6 +51,17 @@ def get_env(cache_dir):
 
 
 def parent_dir(dir_path):
+    """
+    Get the parent dir from of the top directory for a path,
+    regardless of whether the path ends in a filename or not
+
+    > parent_dir('/my/favourite/directory')
+    '/my/favourite'
+
+    > parent_dir('/my/favourite/directory/my-file.txt')
+    '/my/favourite'
+    """
+
     if not path.isdir(dir_path):
         dir_path = path.dirname(dir_path)
 
@@ -42,10 +69,20 @@ def parent_dir(dir_path):
 
 
 def build_url(
-    scheme, domain,
-    port=None, username=None, password=None,
+    domain,
+    scheme='http', port=None, username=None, password=None,
     path='', params='', query='', fragment=''
 ):
+    """
+    Build a URL from its parts
+
+    This is just a proxy for urlunparse,
+    but with more descriptive parameter names
+
+    > build_url(domain='example.com', port=8080, path='users')
+    'http://example.com:8080/users'
+    """
+
     return urlunparse(
         (
             scheme,
@@ -59,6 +96,22 @@ def build_url(
 
 
 def build_url_host(domain, port=None, username=None, password=None):
+    """
+    Build the host part of a URL from a domain, port, username and password
+
+    > build_url_host('example.com')
+    'example.com'
+
+    > build_url_host('example.com', 8080)
+    'example.com:8080'
+
+    > build_url_host('example.com', 8080, 'robin')
+    'robin@example.com:8080'
+
+    > build_url_host('example.com', 8080, 'robin', 'mypassword')
+    'robin:mypassword@example.com:8080'
+    """
+
     host = domain
 
     if port:
@@ -77,13 +130,13 @@ def build_url_host(domain, port=None, username=None, password=None):
     return host
 
 
-def ansible_config(charm_dir, config_data):
-    '''
+def add_ansible_config(charm_dir, config_data):
+    """
     Collect config settings from an ansible role
     by reading both 'defaults/main.yml'
     and 'vars/main.yml'
     and appending their data to config_data
-    '''
+    """
 
     # Local imports - 'cos they weren't ready earlier
     from jinja2 import Environment, FileSystemLoader
@@ -92,34 +145,39 @@ def ansible_config(charm_dir, config_data):
     wsgi_role_path = path.join(charm_dir, 'roles', 'wsgi-app')
     template_env = Environment(loader=FileSystemLoader(wsgi_role_path))
 
-    config_data = add_yaml_to_config(
+    config_data = update_from_yaml_template(
         'defaults/main.yml', template_env, config_data
     )
 
-    config_data = add_yaml_to_config(
+    config_data = update_from_yaml_template(
         'vars/main.yml', template_env, config_data
     )
 
     return config_data
 
 
-def add_yaml_to_config(template_path, template_env, config_data):
+def update_from_yaml_template(template_path, parser_env, data):
+    """
+    Given a path to a yaml file and a template parser environment
+    add all data in the yaml file to the provided data dictionary
+    """
+
     import yaml
     from jinja2 import meta, UndefinedError
 
     # Find variables in template
-    content = template_env.loader.get_source(template_env, template_path)
-    template_parser = template_env.parse(content[0])
-    template_variables = meta.find_undeclared_variables(template_parser)
+    content = parser_env.loader.get_source(parser_env, template_path)
+    parsed_template = parser_env.parse(content[0])
+    template_variables = meta.find_undeclared_variables(parsed_template)
 
     # Find undeclared variables not already in context
-    undefined_variables = list(set(template_variables) - set(config_data))
+    undefined_variables = list(set(template_variables) - set(data))
 
     # Parse template
-    template = template_env.get_template(template_path)
-    yaml_content = template.render(config_data)
+    template = parser_env.get_template(template_path)
+    yaml_content = template.render(data)
     new_variables = yaml.load(yaml_content)
-    config_data.update(new_variables)
+    data.update(new_variables)
 
     # If any new variables were in the undeclared variables
     list_intersection = filter(
@@ -127,8 +185,34 @@ def add_yaml_to_config(template_path, template_env, config_data):
     )
 
     if list_intersection:
-        add_yaml_to_config(template_path, template_env, config_data)
+        data = update_from_yaml_template(template_path, parser_env, data)
     elif undefined_variables:
         raise UndefinedError('Undefined variables: ' + str(undefined_variables))
 
-    return config_data
+    return data
+
+
+def items_are_not_empty(test_dictionary, items_to_test):
+    """
+    For a list of keys, check all items in a dictionary with those key names
+    are not empty
+
+    > items_are_not_empty({'a': 'hi'}, ['a'])
+    True
+
+    > items_are_not_empty({'a': 'hi', 'b': ''}, ['a', 'b'])
+    False
+
+    > items_are_not_empty({'a': 'hi', 'b': ''}, ['a', 'c'])
+    False
+
+    > items_are_not_empty({'a': 'hi', 'b': '', 'c': 1}, ['a', 'c'])
+    True
+
+    """
+
+    # A list of booleans representing whether items evaluate to True
+    item_booleans = [bool(test_dictionary.get(k)) for k in items_to_test]
+
+    # Return True only if all items are present and True
+    return reduce(lambda x, y: x and y, item_booleans)
--- hooks/hooks.py
+++ hooks/hooks.py
@@ -7,22 +7,36 @@ from os import path, mkdir
 from datetime import datetime
 
 # Local
-import charmhelpers.contrib.ansible
+import sh
 from helpers import (
-    build_url, parent_dir, ansible_config,
-    set_env_var, get_env, set_env
+    build_url, parent_dir, add_ansible_config,
+    update_property_in_json_file, parse_json_file,
+    items_are_not_empty
 )
+import charmhelpers.contrib.ansible
 from charmhelpers.core.hookenv import (
     relation_get, relation_set,
     open_port, close_port,
-    config, local_unit,
-    relation_ids
+    relation_ids, relations,
+    config, local_unit
 )
 from charmhelpers.core.host import log
 
 # Globals (unfortunately)
 charm_dir = parent_dir(__file__)
 cache_dir = path.join(charm_dir, 'charm_cache')
+env_file_path = path.join(cache_dir, 'env.json')
+
+
+def ansible_config():
+    """
+    Build ansible config data, extending current config_data
+    and with the local unit name
+    """
+
+    config_data = config()
+    config_data['local_unit'] = local_unit()
+    return add_ansible_config(charm_dir, config_data)
 
 # Create the hooks helper which automatically registers the
 # required hooks based on the available tags in your playbook.
@@ -33,100 +47,11 @@ hooks = charmhelpers.contrib.ansible.AnsibleHooks(
 )
 
 
-@hooks.hook('wsgi-file-relation-changed')
-def wsgi_relation_joined_changed():
-    template_vars = config()
-    template_vars['local_unit'] = local_unit()
-    log('Config options: ' + str(template_vars))
-    ansible_data = ansible_config(charm_dir, template_vars)
-    log('Ansible config options: ' + str(ansible_data))
-
-    log_file_path = path.join(
-        ansible_data['log_dir'],
-        ansible_data['app_label'] + '-access.log'
-    )
-
-    env_dictionary = get_env(cache_dir)
-    env_list = ["{0}={1}".format(k, v) for k, v in env_dictionary.items()]
-    env_string = " ".join(env_list)
-
-    wsgi_relation_settings = {
-        'project_name': ansible_data.get('app_label', ''),
-        'working_dir': path.join(ansible_data.get('code_dir', ''), 'current'),
-        'python_path': ansible_data.get('python_path', ''),
-        'wsgi_user': ansible_data.get('wsgi_user', ''),
-        'wsgi_group': ansible_data.get('wsgi_group', ''),
-        'port': ansible_data.get('listen_port', ''),
-        'wsgi_access_logfile': log_file_path,
-        'wsgi_wsgi_file': ansible_data.get('wsgi_application', ''),
-        'wsgi_extra': '--error-logfile=' + log_file_path,
-        'env_extra': env_string,
-        'timestamp': datetime.now().isoformat()
-    }
-
-    log('wsgi relation settings: ' + str(wsgi_relation_settings))
-
-    # Set these settings on any wsgi-file relations
-    for relation_id in relation_ids('wsgi-file'):
-        relation_set(
-            relation_id=relation_id,
-            **wsgi_relation_settings
-        )
-
-    log('Relation settings: ' + str(relation_get()))
-
-    open_port(ansible_data['listen_port'])
-
-
-@hooks.hook('wsgi-relation-broken')
-def wsgi_relation_broken():
-    template_vars = config()
-    template_vars['local_unit'] = local_unit()
-    ansible_data = ansible_config(charm_dir, template_vars)
-
-    close_port(ansible_data['listen_port'])
-
-
-@hooks.hook('pgsql-relation-broken')
-def pgsql_relation_broken():
-    env_vars = get_env(cache_dir)
-
-    if 'DATABASE_URL' in env_vars:
-        del env_vars['DATABASE_URL']
-
-        set_env(env_vars)
-
-        # Reset wsgi relation settings
-        wsgi_relation_joined_changed()
-
-
-@hooks.hook('pgsql-relation-joined', 'pgsql-relation-changed')
-def pgsql_relation():
-    database_name = relation_get("database")
-    database_host = relation_get("host")
-
-    if database_name and database_host:
-        # Prepare the charm for postgres database relation
-        # By putting database settings in environment variables
-
-        database_url = build_url(
-            scheme='postgresql',
-            domain=database_host,
-            port=relation_get("port"),
-            username=relation_get("user"),
-            password=relation_get("password"),
-            path=database_name
-        )
-
-        set_env_var(cache_dir, 'DATABASE_URL', database_url)
-
-        # Reset wsgi relation settings
-        wsgi_relation_joined_changed()
-
-
 @hooks.hook('install', 'upgrade-charm')
 def install():
-    """Install ansible.
+    """
+    - Install ansible
+    - Create the cache directory
 
     The hook() helper decorating this install function ensures that after this
     function finishes, any tasks in the playbook tagged with install are
@@ -141,6 +66,133 @@ def install():
 
     # Setup ansible
     charmhelpers.contrib.ansible.install_ansible_support(from_ppa=True)
+
+
+@hooks.hook('config-changed')
+def update_target():
+    """
+    Run the "update-charm" make target within the project
+    """
+
+    config_data = ansible_config()
+
+    required_configs = [
+        'build_label',
+        'archive_filename',
+        'code_dir',
+        'update_make_target'
+    ]
+
+    # Check all required configs are set
+    if items_are_not_empty(config_data, required_configs):
+        env_vars = parse_json_file(env_file_path)
+
+        # Execute make target with all environment variables
+        sh.make(
+            config_data['update_make_target'],
+            directory=path.join(config_data.get('code_dir', ''), 'current'),
+            _env=env_vars
+        )
+
+
+@hooks.hook('pgsql-relation-changed', 'config-changed')
+def pgsql_relation():
+    for relation_id in relation_ids('pgsql'):
+        database_name = relation_get(
+            "database",
+            rid=relation_id
+        )
+        database_host = relation_get(
+            "host",
+            rid=relation_id
+        )
+
+        if 'pgsql' in relations() and database_name and database_host:
+            # Prepare the charm for postgres database relation
+            # By putting database settings in environment variables
+
+            database_url = build_url(
+                scheme='postgresql',
+                domain=database_host,
+                port=relation_get("port"),
+                username=relation_get("user"),
+                password=relation_get("password"),
+                path=database_name
+            )
+
+            update_property_in_json_file(
+                env_file_path, 'DATABASE_URL', database_url
+            )
+
+            # Reset wsgi relation settings
+            wsgi_relation()
+
+
+@hooks.hook('pgsql-relation-broken')
+def pgsql_relation_broken():
+    env_vars = parse_json_file(env_file_path)
+
+    if 'DATABASE_URL' in env_vars:
+        del env_vars['DATABASE_URL']
+
+        update_property_in_json_file(env_file_path, env_vars)
+
+        # Reset wsgi relation settings
+        wsgi_relation()
+
+
+@hooks.hook('wsgi-file-relation-changed', 'config-changed')
+def wsgi_relation():
+    config_data = ansible_config()
+
+    log_file_path = path.join(
+        config_data['log_dir'],
+        config_data['app_label'] + '-access.log'
+    )
+
+    env_dictionary = parse_json_file(env_file_path)
+    env_list = ["{0}={1}".format(k, v) for k, v in env_dictionary.items()]
+    env_string = " ".join(env_list)
+
+    wsgi_relation_settings = {
+        'project_name': config_data.get('app_label', ''),
+        'working_dir': path.join(config_data.get('code_dir', ''), 'current'),
+        'python_path': config_data.get('python_path', ''),
+        'wsgi_user': config_data.get('wsgi_user', ''),
+        'wsgi_group': config_data.get('wsgi_group', ''),
+        'port': config_data.get('listen_port', ''),
+        'wsgi_access_logfile': log_file_path,
+        'wsgi_wsgi_file': config_data.get('wsgi_application', ''),
+        'wsgi_extra': '--error-logfile=' + log_file_path,
+        'env_extra': env_string,
+        'timestamp': datetime.now().isoformat()
+    }
+
+    # Set these settings on any wsgi-file relations
+    for relation_id in relation_ids('wsgi-file'):
+        log(
+            'Setting wsgi-file relation settings: '
+            + str(wsgi_relation_settings)
+        )
+
+        relation_set(
+            relation_id=relation_id,
+            **wsgi_relation_settings
+        )
+
+    open_port(config_data['listen_port'])
+
+
+@hooks.hook('wsgi-relation-broken')
+def wsgi_relation_broken():
+    """
+    When WSGI relation (e.g.: gunicorn) goes away
+
+    """
+
+    config_data = ansible_config()
+
+    close_port(config_data['listen_port'])
 
 
 if __name__ == "__main__":
